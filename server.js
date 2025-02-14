edb571b1989fb819ad7240e5cdd39416879165ec
Jad <JadHanna603@gmail.com>
2025-02-13 17:53:53 -0800

    multiple instances

--- server.js
+++ server.js
@@ -202,4 +202,6 @@ app.get('/:pagename', async (req, res) => {
 });
 
 // Start the server
-app.listen(port, '0.0.0.0', () => console.log(`Server running at http://0.0.0.0:${port}`));
+if (require.main === module) {
+    app.listen(port, '0.0.0.0', () => console.log(`🚀 Server running at http://0.0.0.0:${port}`));
+}
